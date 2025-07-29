48e15bc85969c4fe055fd7af7ead43925d00ca58
Shaban Najaat <najaatmohammedshaban@gmail.com>
2025-07-29 18:27:56 +0000

    Add files via upload

---
+++ arithemtic_operation.py
@@ -0,0 +1,19 @@
+def perform_operation(num1: float, num2: float, operation: str):
+    operation = operation.lower.()
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtact':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+        return "Error: Division by zero!"
+git 1s-remote --heads origin
+
+
+
+
+
+
+uywtscfhgj
