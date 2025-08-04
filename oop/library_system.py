cf0e64a9a359988760c45891cb4d303c4339afa6
Shaban Najaat <najaatmohammedshaban@gmail.com>
2025-08-04 14:56:45 +0100

    Update library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -26,9 +26,11 @@ class Library:
     def __init__(self):
         self.books = []
 
- def add_book(self, book):
+    def add_book(self, book):
         self.books.append(book)
 
-    def list_books(self):
+ def list_books(self):
         for book in self.books:
             print(book)
+
+
