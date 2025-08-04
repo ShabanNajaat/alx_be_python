f0d72a6feba8249787672353857364e52329796b
Shaban Najaat <najaatmohammedshaban@gmail.com>
2025-08-04 14:43:17 +0100

    Add book_class.py

---
+++ oop/book_class.py
@@ -0,0 +1,38 @@
+class Book:
+    def __init__(self, title: str, author: str, year: int):
+        """
+        Initializes a Book instance with title, author, and year.
+
+        Args:
+            title (str): The title of the book.
+            author (str): The author of the book.
+            year (int): The publication year of the book.
+        """
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __del__(self):
+        """
+        Prints a message upon object deletion.
+        """
+        print(f"Deleting {self.title}")
+
+    def __str__(self):
+        """
+        Returns a string representation of the book in a human-readable format.
+
+        Returns:
+            str: A string in the format "(title) by (author), published in (year)".
+ """
+        return f"{self.title} by {self.author}, published in {self.year}"
+
+    def __repr__(self):
+        """
+        Returns a string that would recreate the Book instance.
+
+        Returns:
+            str: A string in the format "Book('title', 'author', year)".
+        """
+        return f"Book('{self.title}', '{self.author}', {self.year})"
+
